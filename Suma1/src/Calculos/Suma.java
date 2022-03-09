d6dc9d93e489e1c9cc4b5adac32c91f73560a1fa
Getze <alebk201@gmail.com>
2022-03-09 11:23:16 -0600

    terminado

--- Suma1/src/Calculos/Suma.java
+++ Suma1/src/Calculos/Suma.java
@@ -1,5 +1,17 @@
 package Calculos;
 
-public class Suma {
-
+import java.util.Scanner;
+ 
+class Suma {
+ 
+   public static void main( String[] args ) {
+      int a, b, c = 0;
+      Scanner s = new Scanner( System.in );
+      System.out.println( "Introduce dos números: ");
+      a = s.nextInt();
+      b = s.nextInt();
+      c = a + b;
+      System.out.println( "La suma de a y b es: " + c );
+   }
+ 
 }
